665793c2a8a2d77aa3f79d6a72e2c5be791a95e6
Rayson Zheng <raysonzheng10@gmail.com>
2025-04-08 22:42:00 -0400

    made a few api endpoints with dummy data for now

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 # import firebase_admin
 # from firebase_admin import credentials, firestore
 import scraper  # You will create this module
+from dummy_data import *
 
 app = Flask(__name__)
 CORS(app)  # Enable cross-origin requests
@@ -26,6 +27,36 @@ def get_meal_data():
         return jsonify({"status": "success", "data": meal_data})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+
+@app.route("/api/get-user-transactions", methods=["POST"])
+def get_user_transactions():
+    data = request.json
+    dummy = data.get("dummy", True)  # Default to true for now
+
+    if dummy:
+        return jsonify({"status": "success", "data": dummy_transactions})
+
+    # TODO: implement actual transaction fetching
+
+@app.route("/api/get-user-profile", methods=["POST"])
+def get_user_profile():
+    data = request.json
+    dummy = data.get("dummy", True)
+
+    if dummy:
+        return jsonify({"status": "success", "data": dummy_user_data})
+
+    # TODO: implement actual user data fetching
+
+@app.route("/api/get-user-accounts", methods=["POST"])
+def get_user_accounts():
+    data = request.json
+    dummy = data.get("dummy", True)
+
+    if dummy:
+        return jsonify({"status": "success", "data": dummy_accounts_data})
+
+    # TODO: implement actual user data fetching
 
 if __name__ == "__main__":
     app.run(debug=True)
