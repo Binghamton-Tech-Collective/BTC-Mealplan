237c8d6cfbb53363b5affcddaf8e43d77bd7963b
jz391 <128315339+jz391@users.noreply.github.com>
2025-04-14 15:26:27 -0400

    Login & account scraping
    login interface set up with error handling and implemented web scraping for accounts if login was successful.
    
    Will need to update error handling to return json errors instead in the future; still working on scraping account transactions

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
+from InquirerPy import inquirer
+
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+# from flask_cors import CORS
+
+import scraper  # You will create this module
+import app
+
 # import firebase_admin
 # from firebase_admin import credentials, firestore
-import scraper  # You will create this module
 
-app = Flask(__name__)
-CORS(app)  # Enable cross-origin requests
-
+# app = Flask(__name__)
+# CORS(app)  # Enable cross-origin requests
 
 # Can ignore Firebase for now
 
@@ -15,17 +19,36 @@ CORS(app)  # Enable cross-origin requests
 # firebase_admin.initialize_app(cred)
 # db = firestore.client()
 
-@app.route("/api/meal-data", methods=["POST"])
+# @app.route("/api/meal-data", methods=["POST"])
 def get_meal_data():
-    data = request.json
-    username = data.get("username")
-    password = data.get("password")
+    username = inquirer.text(
+        message="Enter your username:"
+    ).execute()
 
-    try:
-        meal_data = scraper.fetch_meal_data(username, password)
-        return jsonify({"status": "success", "data": meal_data})
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+    password = inquirer.secret(
+        message="Enter your password:"
+    ).execute()
+    
+    session = scraper.new_session()
+    loginPage = scraper.fetch_login_page(username, password, session)
+    accounts = scraper.fetch_accounts_data(loginPage, session)
 
+    #See scraper.py's Account class for info on retrieving data through python, otherwise jsonify should do it for you
+
+    # print("\Your current accounts:")
+    # for acc in accounts:
+    #     print("\t"+str(acc))
+
+    #scraper.load_account_transaction_history(accounts[0], session)
+    
+    return jsonify({"status": "success", "data": accounts}) #working on error handling later
+    # try:
+    #     meal_data = scraper.fetch_meal_data(username, password)
+    #     return jsonify({"status": "success", "data": meal_data})
+    # except Exception as e:
+    #     return jsonify({"status": "error", "message": str(e)}), 500
+
+def run(debug):
+    get_meal_data()
 if __name__ == "__main__":
     app.run(debug=True)
