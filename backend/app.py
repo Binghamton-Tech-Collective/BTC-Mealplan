14f0eae34970f282541bd9deb0fe0bf694a0103e
Rayson Zheng <raysonzheng10@gmail.com>
2025-04-16 21:18:44 -0400

    redid dummy endpoints and created endpoint for getting user accounts

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,5 @@
-from InquirerPy import inquirer
-
 from flask import Flask, jsonify, request
-# from flask_cors import CORS
+from flask_cors import CORS
 
 import scraper  # You will create this module
 import app
@@ -11,8 +9,8 @@ import app
 import scraper  # You will create this module
 from dummy_data import *
 
-# app = Flask(__name__)
-# CORS(app)  # Enable cross-origin requests
+app = Flask(__name__)
+CORS(app)  # Enable cross-origin requests
 
 # Can ignore Firebase for now
 
@@ -21,64 +19,54 @@ from dummy_data import *
 # firebase_admin.initialize_app(cred)
 # db = firestore.client()
 
-# @app.route("/api/meal-data", methods=["POST"])
-def get_meal_data():
-    username = inquirer.text(
-        message="Enter your username:"
-    ).execute()
-
-    password = inquirer.secret(
-        message="Enter your password:"
-    ).execute()
-    
-    session = scraper.new_session()
-    loginPage = scraper.fetch_login_page(username, password, session)
-    accounts = scraper.fetch_accounts_data(loginPage, session)
-
-    #See scraper.py's Account class for info on retrieving data through python, otherwise jsonify should do it for you
-
-    # print("\Your current accounts:")
-    # for acc in accounts:
-    #     print("\t"+str(acc))
-
-    #scraper.load_account_transaction_history(accounts[0], session)
-    
-    return jsonify({"status": "success", "data": accounts}) #working on error handling later
-    # try:
-    #     meal_data = scraper.fetch_meal_data(username, password)
-    #     return jsonify({"status": "success", "data": meal_data})
-    # except Exception as e:
-    #     return jsonify({"status": "error", "message": str(e)}), 500
-
-@app.route("/api/get-user-transactions", methods=["POST"])
-def get_user_transactions():
-    data = request.json
-    dummy = data.get("dummy", True)  # Default to true for now
-
-    if dummy:
-        return jsonify({"status": "success", "data": dummy_transactions})
-
-    # TODO: implement actual transaction fetching
-
-@app.route("/api/get-user-profile", methods=["POST"])
-def get_user_profile():
-    data = request.json
-    dummy = data.get("dummy", True)
-
-    if dummy:
-        return jsonify({"status": "success", "data": dummy_user_data})
-
-    # TODO: implement actual user data fetching
-
 @app.route("/api/get-user-accounts", methods=["POST"])
-def get_user_accounts():
+def get_meal_data():
+    # grab user/pass from request body and verify that they are nonempty
     data = request.json
-    dummy = data.get("dummy", True)
+    username = data.get("username")
+    password = data.get("password")
 
-    if dummy:
-        return jsonify({"status": "success", "data": dummy_accounts_data})
+    if not username or not password:
+        return jsonify({"status": "error", "message": "Username and password required"}), 400
 
-    # TODO: implement actual user data fetching
+
+    session = scraper.new_session()
+    try:
+        loginPage = scraper.fetch_login_page(username, password, session)
+        accounts = scraper.fetch_accounts_data(loginPage, session)
+
+        data = list()
+
+        # jsonify can't parse the Account class object, so we manually convert to dictionary instead
+        # depending on how future things are implemented, we should factor this function into a utils.py
+        for acc in accounts:
+            data.append({
+                "name" : acc.name,
+                "balance" : acc.balance,
+                "accountId" : acc.accountId,
+                "transactions" : acc.transactions,
+            })
+
+        return jsonify({"status": "success", "data": data})
+    except Exception as e:
+        # TODO: currently this will never trigger b/c scraper.py just kills the program altogether
+        # we should raise errors instead so that we can actually send that error message back to the endpoint user
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+
+# endpoints for dummy data
+@app.route("/api/dummy/get-transactions", methods=["GET"])
+def get_user_transactions():
+    return jsonify({"status": "success", "data": dummy_transactions})
+
+@app.route("/api/dummy/get-profile", methods=["GET"])
+def get_user_profile():
+    return jsonify({"status": "success", "data": dummy_user_data})
+
+@app.route("/api/dummy/get-accounts", methods=["GET"])
+def get_user_accounts():
+    return jsonify({"status": "success", "data": dummy_accounts_data})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
